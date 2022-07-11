2e5e3e7754d91cfe3f7d1d5b3828eb7a50c91e7b
Jair Roca <jair.roca@cu.ucsg.edu.ec>
2022-07-11 00:52:01 -0500

    Cartas implementadas

---
+++ app/Http/Controllers/API/CartasController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Carta;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class CartasController extends Controller
+{
+    public function show($id)
+    {
+        $tipo = Carta::findOrFail($id);
+        return response($tipo, 200);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'coleccion_id' => 'required|exists:colecciones,id',
+            'descripcion' => 'required|string|max:255',
+            'condicion' => 'required|string|max:1',
+            'cantidad' => 'required|numeric',
+            'numeracion' => 'required|string'
+        ]);
+        $tipo = Carta::create([
+            'coleccion_id' => $request->input('coleccion_id'),
+            'descripcion' => $request->input('descripcion'),
+            'condicion' => $request->input('condicion'),
+            'cantidad' => $request->input('cantidad'),
+            'numeracion' => $request->input('numeracion')
+        ]);
+
+        return response($tipo, 201);
+
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'coleccion_id' => 'required|exists:colecciones,id',
+            'descripcion' => 'required|string|max:255',
+            'condicion' => 'required|string|max:1',
+            'cantidad' => 'required|numeric',
+            'numeracion' => 'required|string'
+        ]);
+        $tipo = Carta::findOrFail($id);
+        $tipo->update([
+            'descripcion' => $request->input('descripcion'),
+            'condicion' => $request->input('condicion'),
+            'cantidad' => $request->input('cantidad'),
+            'numeracion' => $request->input('numeracion')
+        ]);
+
+        return response($tipo, 201);
+    }
+    public function destroy($id)
+    {
+        $tipo = Carta::findOrFail($id);
+        $tipo->delete();
+        return response(['message' => 'Carta eliminada'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CartasController;
 use App\Http\Controllers\API\ColeccionController;
 use App\Http\Controllers\API\PerfilController;
 use App\Http\Controllers\API\TipoColeccionController;
@@ -29,6 +30,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
         'show', 'store', 'update', 'destroy'
     ]);
     Route::resource('tipo-coleccion', TipoColeccionController::class)->only([
+        'show', 'store', 'update', 'destroy'
+    ]);
+    Route::resource('carta', CartasController::class)->only([
         'show', 'store', 'update', 'destroy'
     ]);
 
